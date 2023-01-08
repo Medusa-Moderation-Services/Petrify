b25cf74ac9946b64026bd79402d83676d8aab32d
Ophidie <basilicacorporationceo@gmail.com>
2023-01-07 20:59:15 -0500

    Added Updated Data to the data module

--- src/Petrify-Data/MainModule.lua
+++ src/Petrify-Data/MainModule.lua
@@ -1,5 +1,70 @@
 local HTTPService = game:GetService("HttpService")
 
-local Data = ""
-
+-- TODO Test to ensure this data is properly decoded and works as intended.
+local Data = [[
+    {
+    "ModerationEntries": {
+      "\"0000000003\"": {
+        "AppealStatus": "Un-Appealed",
+        "BanType": "Player",
+        "Description": "N/A",
+        "Flags": [
+          "Exploiting",
+          "Undesirable"
+        ],
+        "ModeratorUserId": 1794584685,
+        "UserId": 13130330
+      },
+      "NextModerationEntryNumber": 1
+    },
+    "PlayerEntries": {
+      "964361": {
+        "DangerLevel": "High",
+        "Flags": [
+          "nil"
+        ],
+        "InvalidModerationEntries": [
+          "nil"
+        ],
+        "ModerationEntries": [
+          "0000000002"
+        ],
+        "Notes": [
+          "nil"
+        ]
+      },
+      "13130330": {
+        "DangerLevel": "High",
+        "Flags": [
+          "nil"
+        ],
+        "InvalidModerationEntries": [
+          "nil"
+        ],
+        "ModerationEntries": [
+          "0000000003"
+        ],
+        "Notes": [
+          "nil"
+        ]
+      },
+      "1794584685": {
+        "DangerLevel": "Low",
+        "Flags": [
+          "nil"
+        ],
+        "InvalidModerationEntries": [
+          "nil"
+        ],
+        "ModerationEntries": [
+          "nil",
+          "0000000004"
+        ],
+        "Notes": [
+          "nil"
+        ]
+      }
+    }
+  }
+]]
 return HTTPService:JSONDecode(Data)
